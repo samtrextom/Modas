db2f814d7c4502fb2f3b00c52e3fcf67d4fb79c4
samtrextom <samtrextom@gmail.com>
2020-01-20 21:18:13 -0600

    ClassONe

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Modas.Models;
 
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
-        public String Index() => "This is words";
+        private IEventRepository repository;
+        public HomeController(IEventRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Events);
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Modas.Models;
 
 namespace Modas
 {
@@ -15,18 +16,16 @@ namespace Modas
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IEventRepository, FakeEventRepository>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
             app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
         }
     }
 }
