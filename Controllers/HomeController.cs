e9a6b1421a362a7d06ecf7caf3316f4c89a5a700
samtrextom <samtrextom@gmail.com>
2020-01-20 19:25:42 -0600

    StartpClass + AddHomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index() => "This is words";
+    }
+}
