8b3177bf83295ab4ea4e7ffb44bd37f339187515
Samuel Tom <stom@my.wctc.edu>
2020-02-03 21:17:12 -0600

    Class 3

---
+++ Models/EFEventRespository.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace Modas.Models
+{
+    public class EFEventRepository : IEventRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFEventRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Event> Events => context.Events;
+        public IQueryable<Location> Locations => context.Locations;
+
+        public Event AddEvent(Event evt)
+        {
+            context.Add(evt);
+            context.SaveChanges();
+            return evt;
+        }
+
+        public Event UpdateEvent(Event evt)
+        {
+            Event Event = context.Events.FirstOrDefault(e => e.EventId == evt.EventId);
+            Event.TimeStamp = evt.TimeStamp;
+            Event.Flagged = evt.Flagged;
+            Event.LocationId = evt.LocationId;
+            context.SaveChanges();
+            return Event;
+        }
+
+        public void DeleteEvent(int eventId)
+        {
+            Event evt = context.Events.FirstOrDefault(e => e.EventId == eventId);
+            context.Events.Remove(evt);
+            context.SaveChanges();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Modas.Models;
 
@@ -12,20 +14,43 @@ namespace Modas
 {
     public class Startup
     {
+        // this exposes the connection string in appsettings.json
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IEventRepository, FakeEventRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:Modas:ConnectionString"]));
+            services.AddTransient<IEventRepository, EFEventRepository>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "Page{page}",
+                    defaults: new { Controller = "Home", action = "Index" });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
             app.UseStaticFiles();
+            //SeedData.EnsurePopulated(app);
         }
     }
 }
